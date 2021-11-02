b9e9a6f810b3b92007b1011867aa3793ecf53b90
Ryomanba <datemanba3@gmail.com>
2021-11-02 19:54:22 +0900

    simple version

--- raytracing.cpp
+++ raytracing.cpp
@@ -10,6 +10,7 @@ int main()
 	std::cout << "P3\n" << image_width << ' ' << image_height << "\n255\n";
 
 	for (int j = image_height - 1; j >= 0; j--) {
+		std::cerr << "\rScanlines remaingin: " << j << ' ' << std::flush;
 		for (int i = 0; i < image_width; i++) {
 			auto r = double(i) / (image_width - 1); // i=255の時, r=255
 			auto g = double(j) /(image_height - 1); // j=255の時, g=255
@@ -23,4 +24,5 @@ int main()
 			std::cout << ir << ' ' << ig << ' ' << ib << '\n';
 		}
 	}
+	std::cerr << "\nDone.\n";
 }
--- vec3.h
+++ vec3.h
@@ -42,10 +42,6 @@ class vec3 {
 		}
 
 		double length_squared() const {
-			return sqrt(length_squared());
-		}
-
-		double length_squared() const {
 			return e[0]*e[0] + e[1]*e[1] + e[2]*e[2];
 		}
 
@@ -73,6 +69,10 @@ inline vec3 operator-(const vec3 &u, const vec3 &v) {
 
 inline vec3 operator*(const vec3 &u, const vec3 &v) {
 	return vec3(u.e[0] * v.e[0], u.e[1] * v.e[1], u.e[2] * v.e[2]);
+}
+
+inline vec3 operator*(double t, const vec3 &v) {
+	return vec3(t*v.e[0], t*v.e[1], t*v.e[2]);
 }
 
 inline vec3 operator*(const vec3 &v, double t) {
