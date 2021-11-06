f39f3bda5f74592718c4bb1c32b3ab541b7a33e7
Ryomanba <datemanba3@gmail.com>
2021-11-06 23:41:15 +0900

    アンチエイリアシング

--- main.cpp
+++ main.cpp
@@ -3,7 +3,7 @@
 #include "color.h"
 #include "hittable_list.h"
 #include "sphere.h"
-//#include "vec3.h"
+#include "camera.h"
 
 #include <iostream>
 /*
@@ -54,6 +54,7 @@ int main()
 	const auto aspect_ratio = 16.0 / 9.0; // 画面比率
 	const int image_width = 400;
 	const int image_height = static_cast<int>(image_width / aspect_ratio);
+	const int samples_per_pixel = 100;
 
 	// World
 	hittable_list world;
@@ -61,14 +62,15 @@ int main()
 	world.add(make_shared<sphere>(point3(0, -100.5, -1), 100));
 
 	// Camera
-	auto viewport_height = 2.0; // スクリーンの縦
-	auto viewport_width = aspect_ratio * viewport_height; // 16.0/9.0*2.0 = 3.5555555555555554
-	auto focal_length = 1.0; // 焦点距離
+	camera cam;
+//	auto viewport_height = 2.0; // スクリーンの縦
+//	auto viewport_width = aspect_ratio * viewport_height; // 16.0/9.0*2.0 = 3.5555555555555554
+//	auto focal_length = 1.0; // 焦点距離
 
-	auto origin = point3(0, 0, 0); // 視点
-	auto horizontal = vec3(viewport_width, 0, 0); // 水平, x軸がviewport_width
-	auto vertical = vec3(0, viewport_height, 0); // 垂直, y軸がviewport_height
-	auto lower_left_corner = origin - horizontal / 2 - vertical / 2 - vec3(0, 0, focal_length); // z軸が焦点距離
+//	auto origin = point3(0, 0, 0); // 視点
+//	auto horizontal = vec3(viewport_width, 0, 0); // 水平, x軸がviewport_width
+//	auto vertical = vec3(0, viewport_height, 0); // 垂直, y軸がviewport_height
+//	auto lower_left_corner = origin - horizontal / 2 - vertical / 2 - vec3(0, 0, focal_length); // z軸が焦点距離
 
 	// Render
 	std::cout << "P3\n" << image_width << ' ' << image_height << "\n255\n";
@@ -76,11 +78,14 @@ int main()
 	for (int j = image_height - 1; j >= 0; j--) {
 		std::cerr << "\rScanlines remaining: " << j << ' ' << std::flush;
 		for (int i = 0; i < image_width; i++) {
-			auto u = double(i) / (image_width - 1); // 見ている点の左からの距離
-			auto v = double(j) / (image_height - 1); // 見ている点の下からの距離
-			ray r(origin, lower_left_corner + u * horizontal + v * vertical - origin); // 変化の大きさを求めるためにoriginを引いてる
-			color pixel_color = ray_color(r, world);
-			write_color(std::cout, pixel_color);
+			color pixel_color(0, 0, 0);
+			for (int s = 0; s < samples_per_pixel; s++) {
+				auto u = (i + random_double()) / (image_width - 1); // 見ている点の左からの距離
+				auto v = (j + random_double()) / (image_height - 1); // 見ている点の下からの距離
+				ray r = cam.get_ray(u, v); // 変化の大きさを求めるためにoriginを引いてる
+				pixel_color += ray_color(r, world);
+			}
+			write_color(std::cout, pixel_color, samples_per_pixel);
 		}
 	}
 
--- rtweekend.h
+++ rtweekend.h
@@ -26,5 +26,23 @@ inline double degrees_to_radians(double degrees) {
 
 #include "ray.h"
 #include "vec3.h"
+#include <cstdlib>
+
+inline double random_double() {
+	// Returns a random real in [0,1).
+	return rand() / (RAND_MAX + 1.0);
+}
+
+inline double random_double(double min, double max) {
+	// Returns a random real in [min, max).
+	return min + (max - min) * random_double();
+}
+
+// xがmin未満だったらmin, 大きかったらmaxを返す
+inline double clamp(double x, double min, double max) {
+	if (x < min) return min;
+	if (x > max) return max;
+	return x;
+}
 
 #endif
