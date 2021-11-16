8eb4a709beb67f2a7fae2d38417262be5c9b9a66
Ryomanba <datemanba3@gmail.com>
2021-11-16 22:37:30 +0900

    update

--- main.cpp
+++ main.cpp
@@ -7,36 +7,6 @@
 #include "material.h"
 
 #include <iostream>
-/*
-double hit_sphere(const point3& center, double radius, const ray& r) {
-	vec3 oc = r.origin() - center; // 視点 - (0,0,-1) スクリーンの真ん中から視点に向かうベクトル
-	auto a = r.direction().length_squared();
-	auto half_b = dot(oc, r.direction());
-	auto c = oc.length_squared()- radius * radius;
-	auto discriminant = half_b * half_b - a * c; // 解の公式のルートの中身
-	if (discriminant < 0) // 球に触れてないとき
-		return -1.0;
-	else
-		return (-half_b - sqrt(discriminant)) / a; // 解の公式で求めた t の値 (-でtが小さい方)
-}
-
-color ray_color(const ray& r) {
-	auto t = hit_sphere(point3(0, 0, -1), 0.5, r);
-	if (t > 0.0)
-	{
-		vec3 N = unit_vector(r.at(t) - vec3(0, 0, -1)); // at=origin + t*direction
-		return 0.5 * color(N.x() + 1, N.y() + 1, N.z() + 1); // スクリーン中心からレイと球の交点
-	}
-	// unit_vectorで方位ベクトルが求まる
-	vec3 unit_direction = unit_vector(r.direction());// r/r.length(sqrt(rx*rx, ry*ry, rz*rz))
-	t = 0.5 * (unit_direction.y() + 1.0); // 0.5*(高さ+1.0) ->上が1,下が0
-	return (1.0 - t) * color(1.0, 1.0, 1.0) + t * color(0.5, 0.7, 1.0); // 白と水色のグラデーション
-	// (1.0 -t) 上が0,下が1に変化
-	// 1.0-t と t が重み
-	// 上に行くにつれて t が大きくなる
-	// 最初はtが大きいが徐々に小さくなる
-}
-*/
 
 color ray_color(const ray& r, const hittable& world, int depth) {
     hit_record rec;
@@ -59,37 +29,70 @@ color ray_color(const ray& r, const hittable& world, int depth) {
     return (1.0-t)*color(1.0, 1.0, 1.0) + t*color(0.5, 0.7, 1.0);
 }
 
+hittable_list random_scene() {
+	hittable_list world;
+
+	auto ground_material = make_shared<lambertian> (color(0.5, 0.5, 0.5));
+	world.add(make_shared<sphere>(point3(0, -1000, 0), 1000, ground_material));
+
+	for (int a = -11; a < 11; a++) {
+		for (int b = -11; b < 11; b++) {
+			auto choose_mat = random_double();
+			point3 center(a + 0.9 * random_double(), 0.2, b + 0.9 * random_double());
+
+			if ((center - point3(4, 0.2, 0)).length() > 0.9) {
+				shared_ptr<material> sphere_material;
+
+				if (choose_mat < 0.8) {
+					// diffuse
+					auto albedo = color::random() * color::random();
+					sphere_material = make_shared<lambertian>(albedo);
+					world.add(make_shared<sphere>(center, 0.2, sphere_material));
+				} else if (choose_mat < 0.95) {
+					// metal
+					auto albedo = color::random(0.5, 1);
+					auto fuzz = random_double(0, 0.5);
+					sphere_material = make_shared<metal>(albedo, fuzz);
+					world.add(make_shared<sphere>(center, 0.2, sphere_material));
+				} else {
+					// glass
+					sphere_material = make_shared<dielectric>(1.5);
+					world.add(make_shared<sphere>(center, 0.2, sphere_material));
+				}
+			}
+		}
+	}
+
+	auto material1 = make_shared<dielectric>(1.5);
+	world.add(make_shared<sphere>(point3(0, 1, 0), 1.0, material1));
+
+	auto material2 = make_shared<lambertian>(color(0.4, 0.2, 0.1));
+	world.add(make_shared<sphere>(point3(-4, 1, 0), 1.0, material2));
+
+	auto material3 = make_shared<metal>(color(0.7, 0.6, 0.5), 0.0);
+	world.add(make_shared<sphere>(point3(4, 1, 0), 1.0, material3));
+
+	return world;
+}
 
 int main()
 {
 	// Image
-	const auto aspect_ratio = 16.0 / 9.0; // 画面比率
-	const int image_width = 400;
+	const auto aspect_ratio = 3.0 / 2.0; // 画面比率
+	const int image_width = 1200;
 	const int image_height = static_cast<int>(image_width / aspect_ratio);
-	const int samples_per_pixel = 100;
+	const int samples_per_pixel = 8; //　ここが多いとかなり時間かかる(1ピクセルあたりのサンプル数)
 	const int max_depth = 50;
 
 	// World
-	hittable_list world;
-
-	// lambertianで拡散反射、metalで鏡面反射
-	auto material_ground = make_shared<lambertian>(color(0.8, 0.8, 0.0));
-	auto material_center = make_shared<lambertian>(color(0.1, 0.2, 0.5));
-	auto material_left   = make_shared<dielectric>(1.5);
-	auto material_right  = make_shared<metal>(color(0.8, 0.6, 0.2), 0.0);
-
-	world.add(make_shared<sphere>(point3( 0.0, -100.5, -1.0), 100.0, material_ground));
-	world.add(make_shared<sphere>(point3( 0.0,    0.0, -1.0),   0.5, material_center));
-	world.add(make_shared<sphere>(point3(-1.0,    0.0, -1.0),   0.5, material_left));
-	world.add(make_shared<sphere>(point3(-1.0,    0.0, -1.0), -0.45, material_left));
-	world.add(make_shared<sphere>(point3( 1.0,    0.0, -1.0),   0.5, material_right));
+	auto world = random_scene();
 
 	// Camera
-	point3 lookfrom(3,3,2);
-	point3 lookat(0,0,-1);
+	point3 lookfrom(13,2,3);
+	point3 lookat(0,0,0);
 	vec3 vup(0,1,0);
-	auto dist_to_focus = (lookfrom - lookat).length();
-	auto aperture = 2.0;
+	auto dist_to_focus = 10.0;
+	auto aperture = 0.1; // ここを増やすことでよりぼかせる
 //	camera cam(point3(-2,2,1), point3(0,0,-1), vec3(0,1,0), 20, aspect_ratio); // 視野角を小さくすることでズームインできる
 	camera cam(lookfrom, lookat, vup, 20, aspect_ratio, aperture, dist_to_focus);
 
